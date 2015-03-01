9e2258d62d7593441ade4418bffc6be9bf6cb1ea
isaiaswebnet <isaiaswebnet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-01 08:56:15 +0000

    Limit Characters in Title
    git-svn-id: https://plugins.svn.wordpress.org/limit-characters-in-title/trunk@1102369 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- limit-characters-in-title.php
+++ limit-characters-in-title.php
@@ -33,7 +33,7 @@ if (!class_exists('Characters_Title')):
         public function lct_insert_meta_box($meta_boxes) {
             $types = get_post_types('', 'names');
             $prefix = 'lct_';
-            $meta_boxes[] = array('id' => 'limit_characters_in_title', 'title' => 'Limite de Caracteres no T&iacute;tulo', 'pages' => $types, 'context' => 'side', 'priority' => 'high', 'show_names' => true, 'fields' => array(array('name' => 'Número de Caracteres', 'desc' => '', 'id' => $prefix . 'charlimit', 'type' => 'text_small', 'attributes' => array(
+            $meta_boxes[] = array('id' => 'limit_characters_in_title', 'title' => 'Limite de Caracteres no T&iacute;tulo', 'pages' => $types, 'context' => 'side', 'priority' => 'high', 'show_names' => true, 'fields' => array(array('name' => 'N&uacute;mero de Caracteres', 'desc' => '', 'id' => $prefix . 'charlimit', 'type' => 'text_small', 'attributes' => array(
             //'placeholder' => 'N&uacute;mero',
             'size' => 3, 'type' => 'number', 'min' => '1', 'max' => '200', 'step' => '1'),), array('name' => 'Retic&ecirc;ncias (...)', 'id' => $prefix . 'suspension_points', 'type' => 'radio', 'options' => array('no' => 'N&atilde;o', 'yes' => 'Sim',), 'default' => 'no',)));
             return $meta_boxes;
